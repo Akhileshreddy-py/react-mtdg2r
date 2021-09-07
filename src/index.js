61e8825b5fd373fd61e16975bbfbb423618cbab5
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-09-07 17:50:10 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-ReactDOM.render(<h1> hello world </h1>, document.getElementById("root"));
+ReactDOM.render(<h1> hello world  akhilesh sir </h1>, document.getElementById("root"));
